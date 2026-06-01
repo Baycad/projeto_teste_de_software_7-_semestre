3652ff4ad7a32c950e1a9d43d092a11afb8be109
Guilherme Soares <guilherme.devtechno@gmail.com>
2026-05-31 23:14:42 -0300

    Delete Baycad RH directory

--- Baycad RH/js/database.js
+++
@@ -1,7 +0,0 @@
-function getFuncionarios() {
-  return JSON.parse(localStorage.getItem("funcionarios")) || [];
-}
-
-function salvarFuncionarios(lista) {
-  localStorage.setItem("funcionarios", JSON.stringify(lista));
-}
